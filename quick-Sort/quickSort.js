ba8b90adbf2b4f679303f6506a21b78a30cbfb94
Robert-William0963 <robertwilliam0963@gmail.com>
2022-08-15 15:21:52 -0300

    First

---
+++ quick-Sort/quickSort.js
@@ -0,0 +1,16 @@
+import {listaLivros} from './array.js'
+
+function quickSort (array, esquerda, direita) {
+    
+    if (array.length > 1) {
+        let varTemp = particiona(array, esquerda, direita)
+    }
+    
+    return array
+}
+
+function particiona(array, esquerda, direita){
+    let pivo = array[Math.floor((esquerda + direita) / 2)]
+}
+
+console.log(quickSort(listaLivros, 0, listaLivros.length-1))
